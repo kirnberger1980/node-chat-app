3bf58e904e1591e588897a6ca8bba531019909f3
Marcell Jobs <marcell.jobs@gmail.com>
2017-09-19 20:45:08 +0200

    Add newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -14,6 +14,17 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected.');
 
+
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage',message);
+    socket.emit('newMessage',{
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().toUTCString()
+    });
+  });
+
   socket.on('disconnect', () => {
     console.log('User disconnected.');
   });
